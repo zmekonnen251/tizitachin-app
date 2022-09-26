c520c470398cf88e5e65dc828f59b425bb616aef
Zelalem <mekonnen.zelalem251@gmail.com>
2022-09-26 14:32:38 +0300

    Import userRoutes and added it to app to use it user routes

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 
 const app = express();
 dotenv.config();
@@ -14,17 +15,18 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
-app.get('/',(req,res)=> {
-  res.send('Hello to Memories API');
+app.get('/', (req, res) => {
+	res.send('Hello to Memories API');
 });
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.CONNECTION_URL)
-  .then(() =>
-    app.listen(PORT, () =>
-      console.log(`Server Running on Port: http://localhost:${PORT}`)
-    )
-  )
-  .catch((error) => console.log(`${error} did not connect`));
+	.connect(process.env.CONNECTION_URL)
+	.then(() =>
+		app.listen(PORT, () =>
+			console.log(`Server Running on Port: http://localhost:${PORT}`)
+		)
+	)
+	.catch((error) => console.log(`${error} did not connect`));
