e6479101daca4e56f0edd2e857016a3cea4a4410
Zelalem <mekonnen.zelalem251@gmail.com>
2022-10-03 19:58:30 +0300

    Add circularProgress with loading state

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -6,15 +6,16 @@ import Post from './Post/Post';
 import classes from './styles';
 
 const Posts = ({ setCurrentId }) => {
-	const posts = useSelector((state) => state.posts);
-	console.log(posts);
+	const { posts, isLoading } = useSelector((state) => state.posts);
 
-	return !posts.length ? (
+	if (!posts.length && !isLoading) return 'No posts';
+
+	return isLoading ? (
 		<CircularProgress />
 	) : (
-		<Grid container spacing={6} sx={classes.mainContainer}>
+		<Grid container spacing={3} sx={classes.mainContainer} alignItems='stretch'>
 			{posts.map((post) => (
-				<Grid key={post._id} item xs={12} sm={6}>
+				<Grid key={post._id} item xs={12} sm={12} md={12} lg={12}>
 					<Post post={post} setCurrentId={setCurrentId} />
 				</Grid>
 			))}
