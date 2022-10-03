9453b61071cf4d92f776c63813c0155fadcf3f6f
Zelalem <mekonnen.zelalem251@gmail.com>
2022-10-03 19:50:31 +0300

    Add fetchPostsBySearch and fetchPost api request end points.

--- client/src/api.js
+++ client/src/api.js
@@ -15,10 +15,11 @@ API.interceptors.request.use((req) => {
 	return req;
 });
 
-export const fetchPosts = () => API.get('/posts');
+export const fetchPost = (id) => API.get(`/posts/${id}`);
+export const fetchPosts = (page) => API.get(`/posts/?page=${page}`);
 export const fetchPostsBySearch = (searchQuery) =>
 	API.get(
-		`/posts/search?searchQuery=${searchQuery.search || 'none'}&tags=${
+		`/posts/search?searchQuery=${searchQuery.searchTerm || 'none'}&tags=${
 			searchQuery.tags
 		}`
 	);
