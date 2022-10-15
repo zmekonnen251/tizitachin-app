7c6e9adda7cc8735aaa6fd00e286b90f8f0b6daa
Zelalem <mekonnen.zelalem251@gmail.com>
2022-10-15 23:41:57 +0300

    Add routes for google singnin,email verification,refresh token

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,21 @@
 import express from 'express';
-
-import { signin, signup } from '../controllers/users.js';
+import auth from '../middleware/auth.js';
+import {
+	signin,
+	signup,
+	signout,
+	googleSignin,
+	refresh,
+	verifyEmail,
+} from '../controllers/users.js';
 
 const router = express.Router();
 
 router.post('/signin', signin);
 router.post('/signup', signup);
+router.get('/signout', signout);
+router.post('/google', googleSignin);
+router.get('/refresh', refresh);
+router.get('/:id/confirmation/:token',verifyEmail);
 
 export default router;
