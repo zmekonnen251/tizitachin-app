d2d84a56db8c0d719dc773a3aa6ab8c041525795
Zelalem <mekonnen.zelalem251@gmail.com>
2022-10-17 18:08:16 +0300

    Put sensative informations to enviromental variables

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -4,19 +4,18 @@ const sendEmail = async (options) => {
 	// 1) Create a transporter
 	try {
 		const transporter = nodemailer.createTransport({
-			host: 'smtp.gmail.com',
-			service: 'gmail',
-			// secure: Boolean(process.env.EMAIL_SECURE),
+			host: process.env.EMAIL_HOST,
+			service: process.env.EMAIL_SERVICE,
 
 			auth: {
-				user: 'zmekonnen59@gmail.com',
-				pass: 'mvonekslmrwyivtz',
+				user: process.env.EMAIL_USERNAME,
+				pass: process.env.EMAIL_PASSWORD,
 			},
 		});
 
 		// Activate in gmail "less secure app" option
 		await transporter.sendMail({
-			from: 'zmekonnen59@gmail.com',
+			from: process.env.EMAIL_USERNAME,
 			to: options.email,
 			subject: options.subject,
 			text: options.message,
